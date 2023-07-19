dc63c8df5be227f745946cade66ca937ada01e32
Luiz Carlos <lsouzapedroso@gmail.com>
2023-07-18 23:06:02 -0300

    Update ApiSponteProvider.php

--- app/Providers/ApiSponteProvider.php
+++ app/Providers/ApiSponteProvider.php
@@ -1,1 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\ServiceProvider;
+
+class ApiSponteProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('api-sponte', function () {
+            return Http::withOptions([
+                'base_uri' => 'https://webservices.sponteweb.com.br/WSApiSponteRest/api/'
+            ])->withHeaders([
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
                 'api_key' => '8D7034',
+                'stream' => true,
+                'version' => '1.0',
+            ]);
+        });
+    }
+
+}
