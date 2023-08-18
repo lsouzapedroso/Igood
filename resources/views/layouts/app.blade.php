62b1038f3c916df34bc3e75d568b407a1880f303
Luiz Carlos <lsouzapedroso@gmail.com>
2023-08-18 02:01:03 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,7 +92,7 @@
             // Inicializa o plugin Tempus Dominus no campo de input
             dateTimePickerField.datetimepicker({
                 locale: 'pt-br',
-                format: 'MM-DD-YYYY HH:mm',
+                format: 'YYYY-MM-DD HH:mm',
                 useCurrent: false,
                 icons: {
                     time: 'fa fa-clock',
