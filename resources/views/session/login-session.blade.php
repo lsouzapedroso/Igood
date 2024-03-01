a9e2c1a86facf6eea40dbb626c74e29b61e25ca0
Luiz Carlos <lsouzapedroso@gmail.com>
2024-03-01 01:24:06 -0300

    Email correction name

--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -17,7 +17,7 @@
                     @csrf
                     <label>Email</label>
                     <div class="mb-3">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="@ccbeuguarapuava.com.br"  value="" aria-label="Email" aria-describedby="email-addon">
+                      <input type="email" class="form-control" name="email" id="email" placeholder="Email"  value="" aria-label="Email" aria-describedby="email-addon">
                       @error('email')
                         <p class="text-danger text-xs mt-2">{{ $message }}</p>
                       @enderror
